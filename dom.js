19948d299adcc9f40efdf6a82eaf6b221eee7aef
aamirabdullahkhan <aamirkhan19994u>
2023-03-28 14:43:57 +0530

    ThirdCommit

--- dom.js
+++ dom.js
@@ -2,3 +2,8 @@
 console.log(document.domain);
 var header=document.getElementById('main-header');
 header.style.borderBottom='solid 5px #000';
+var items=document.getElementsByClassName('list-group-item');
+items[2].style.backgroundColor='green';
+for(var i=0;i<items.length;i++){
+    items[i].style.fontWeight='bold';
+}
