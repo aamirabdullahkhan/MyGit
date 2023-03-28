ada228515ab35dfec731458a09be5a997d1e1caa
aamirabdullahkhan <aamirkhan19994u>
2023-03-28 17:39:41 +0530

    SixthCommit

--- dom.js
+++ dom.js
@@ -15,11 +15,48 @@ header.style.borderBottom='solid 5px #000';
 //   li[i].style.fontWeight='bold';
 // }
 // Queryelector
-var second=document.querySelector('li:nth-child(2)');
-second.style.backgroundColor='green';
-//QuerySelectorAll
-var odd=document.querySelectorAll('li:nth-child(odd)');
+// var second=document.querySelector('li:nth-child(2)');
+// second.style.backgroundColor='green';
+// //QuerySelectorAll
+// var odd=document.querySelectorAll('li:nth-child(odd)');
 
-for(var i=0;i<odd.length;i++){
-  odd[i].style.backgroundColor='grey';
-}
+// for(var i=0;i<odd.length;i++){
+//   odd[i].style.backgroundColor='grey';
+// }
+// TRAVERSING DOM
+var itemList=document.querySelector('#items');
+//parentNode
+itemList.parentNode.style.backgroundColor='grey';
+//childrenNode
+itemList.children[2].style.color='coral';
+var head=document.querySelector('#header-title');
+head.textContent='Item Lister';
+//firstElementChild
+itemList.firstElementChild.textContent='Item 1';
+//lastElementChild
+itemList.lastElementChild.style.color='red';
+//previousElementSibling
+itemList.previousElementSibling.style.color='aqua';
+//createElement
+//createDiv
+var div=document.createElement('div');
+//createClass
+div.className='newDiv';
+//create Id
+div.id='hel';
+//Add Attr
+div.setAttribute('title','heLL');
+//create text Node
+var divText=document.createTextNode('Yeah!!!');
+//adding text to div
+div.appendChild(divText);
+
+//adding text Before
+
+var Before=document.querySelector('header .container');
+var h1=document.querySelector('header h1');
+
+console.log(div);
+
+Before.insertBefore(divText,h1);
+h1.insertBefore(divText,h1);
