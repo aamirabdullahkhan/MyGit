1bbe980914fcf6d4d66e5a3cb58be396c0a671c8
aamirabdullahkhan <aamirkhan19994u>
2023-04-26 08:00:11 +0530

    15

--- main1.js
+++ main1.js
@@ -1,17 +1,25 @@
 // localStorage.setItem('userName', 'obj.name')
-// console.log(localStorage.getItem('userName'))
+ //console.log(localStorage.getItem('userName'))
 function saveToLocalStorage(event){
   event.preventDefault();
   const name=event.target.userName.value;
   const email=event.target.emailId.value;
  // localStorage.setItem('name', name);
-  //localStorage.setItem('email', email);
+ // localStorage.setItem('email', email);
    const obj={
     name,
     email
    }
-   localStorage.setItem( obj.email ,JSON.stringify(obj));
-   showUser(obj)
+   axios.post("https://crudcrud.com/api/99070382efd947bfa4becfaacf5b4255/data", obj)
+   .then((response)=>{
+    showUser(response.data)
+    console.log(response)
+   })
+   .catch((err)=>{
+    console.log(err)
+   })
+   //localStorage.setItem( obj.email ,JSON.stringify(obj));
+   //showUser(obj)
 }
 function showUser(obj){
   const parent=document.getElementById('ListofItems');
